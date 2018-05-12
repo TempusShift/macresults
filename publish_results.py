5fb29927513a9e40b05869f054061d01d80b7f6d
Jeff Rye <macresults@ryeworx.com>
2018-05-12 11:04:17 -0500

    Populate the times for real.

--- publish_results.py
+++ publish_results.py
@@ -30,6 +30,14 @@ def main(args):
                         'to this file.')
     config = parser.parse_args(args)
 
+    results = pd.read_json(config.results_filename,
+                           orient='records', lines=True)
+    print(results.head())
+
+    # FIXME We should dynamically determine this number from the data.
+    config.max_times = 6
+
+    # Set up the templating.
     stache = pystache.Renderer(file_extension=False,
                                partials={})
     stache.partials['style'] = stache.load_template('templates/style.css')
@@ -38,9 +46,7 @@ def main(args):
 
     logo_data_uri = get_image_data_uri('templates/mac-logo-small.png')
 
-    results = pd.read_json(config.results_filename,
-                           orient='records', lines=True)
-    print(results.head())
+    # Prepare the data do go in the template.
 
     # FIXME Need to populate most of this information dynamically.
     options = {
@@ -54,28 +60,9 @@ def main(args):
         'numCones': results['num_cones'].sum()
     }
 
-    options['results'] = [
-        {
-        'trophy': '',
-        'rank': 1,
-        'cls': 'FM',
-        'number': 1,
-        'driver': 'Jason Hobbs',
-        'vehicle': '1999 Novakar J9 Mojave',
-        'times': [
-            { 'time': 30.625 },
-            { 'time': 30.318 },
-            { 'time': 29.579 },
-            { 'time': 34.180 },
-            { 'time': 29.498,
-              'timeClass': 'best' },
-            { 'time': 29.514 }
-        ],
-        'finalTime': 27.020,
-        'diffFromFirst': 0.0,
-        'diffFromPrev': 0.0
-    }]
+    options['results'] = get_results_for_template(results, config)
 
+    # Apply the template and write the result.
     event_results_template = \
       stache.load_template('templates/event-results.html')
     html = stache.render(event_results_template, options)
@@ -96,6 +83,70 @@ def get_image_data_uri(filename):
         base64_data = base64.b64encode(raw_data)
         data_uri = 'data:image/png;base64,' + base64_data.decode('utf-8')
         return data_uri
+
+
+def get_results_for_template(results_df, config):
+    sorted_results = results_df.sort_values(by=['final_time'])
+    results = []
+    rank = 0
+
+    first_time = None
+    prev_time = None
+    for _, row in sorted_results.iterrows():
+        result = {}
+        # FIXME Need trophy indicator.
+        result['trophy'] = ''
+        rank = rank + 1
+        result['rank'] = rank
+
+        result['cls'] = row['class_name']
+        if row['class_index']:
+            result['cls'] = row['class_index'] + '-' + result['cls']
+
+        result['number'] = row['CarNumber']
+        result['driver'] = '%s %s' % (row['FirstName'], row['LastName'])
+        result['vehicle'] = row['Car']
+
+        result['times'] = get_times_for_template(row, config)
+
+        final_time = row['final_time']
+        result['finalTime'] = format_time(final_time)
+        if not first_time:
+            result['diffFromFirst'] = '-'
+            result['diffFromPrev'] = '-'
+            first_time = final_time
+        else:
+            result['diffFromFirst'] = format_time(final_time - first_time)
+            result['diffFromPrev'] = format_time(final_time - prev_time)
+        prev_time = final_time
+
+        results.append(result)
+
+    return results
+
+
+def get_times_for_template(row, config):
+    # Initialize the times array so that we are sure to have a value
+    # in each cell.
+    times = [{} for _ in range(config.max_times)]
+
+    # Loop over the times, formatting the times.
+    pax_times = row['pax_times']
+    best_time_nums = row['best_time_nums']
+
+    for index, pax_time in enumerate(pax_times):
+        time = {}
+        # FIXME Add penalty information.
+        time['time'] = format_time(pax_time)
+        if index + 1 in best_time_nums:
+            time['timeClass'] = 'best'
+        times[index] = time
+
+    return times
+
+
+def format_time(time):
+    return '%0.3f' % time
 
 
 # ------------------------------------------------------------
